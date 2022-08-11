d97a487ce92f3286597baf1394130c89c8dcf5ee
Nate <berrynp@outlook.com>
2022-08-11 13:38:13 -0700

    Small commit, still trying to get these pieces to work together (GitHub desktop has some changed logged, but maybe not all? Should I be working with both open? What am I doing?)

--- Assignment5_1_1/Assignment5_1_1/Form1.cs
+++ Assignment5_1_1/Assignment5_1_1/Form1.cs
@@ -7,8 +7,67 @@ namespace Assignment5_1_1
             InitializeComponent();
 
         }
+        private void button1_Click(object sender, EventArgs e)
+        {
 
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void button5_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void button6_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void button7_Click(object sender, EventArgs e)
+        {
+
+        }
         private void button8_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void button9_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void button0_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void buttonPlus_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void buttonMinus_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void buttonMultiply_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void buttonDivide_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void buttonClear_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void buttonEquals_Click(object sender, EventArgs e)
         {
 
         }
