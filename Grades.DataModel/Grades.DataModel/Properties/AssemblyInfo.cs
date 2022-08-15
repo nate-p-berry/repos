b8ee564c47aa7e7bbd4b0679ae94e43b77acb8fb
Nate Berry <78959107+berrynp76@users.noreply.github.com>
2022-08-15 08:16:27 -0700

    Completed 20483 Lab 7 and moving onto personal projects

--- Grades.DataModel/Grades.DataModel/Properties/AssemblyInfo.cs
+++ Grades.DataModel/Grades.DataModel/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("385ff0a9-48db-4014-b928-07b1635209ff")]
+[assembly: Guid("7bf5936a-4004-4eb5-84ff-664c37eb44c5")]
 
 // Version information for an assembly consists of the following four values:
 //
