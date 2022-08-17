9364abe691aaf7bd570d6947193d0e8973c56c17
Nate Berry <78959107+berrynp76@users.noreply.github.com>
2022-08-17 09:33:59 -0700

    Daily commit 2022-0817
    This is in the morning before I really got underway with much of anything. It occurs to me that this is a dairy of sorts: I won't share my thoughts directly, but a snapshot in time of how I think and what was giving me trouble with this process. Interesting stuff.

---
+++ MSSAProjectTwo/MSSAProjectTwo/Shared/Models/RSSAPIController.cs
@@ -0,0 +1,142 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Globalization;
+using System.Xml.Linq;
+
+namespace MSSAProjectTwo.Shared.Models
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RSSAPIController : ControllerBase
+    {
+        readonly CultureInfo culture = new CultureInfo("en-US");
+
+        [Route("allposts/{authorId}")]
+        [HttpGet]
+        public AuthorPosts AllPosts(string authorId)
+        {
+            AuthorPosts authorPosts = new AuthorPosts();
+            try
+            {
+                XDocument doc = XDocument.Load("https://www.c-sharpcorner.com/members/" + authorId + "/rss");
+                var entries = from item in doc.Root.Descendants().First(i => i.Name.LocalName == "channel").Elements().Where(i =>
+                               i.Name.LocalName == "item")
+                              select new Feed
+                              {
+                                  Content = item.Elements().First(i => i.Name.LocalName == "description").Value,
+                                  Link = item.Elements().First(i => i.Name.LocalName == "link").Value.StartsWith("/") ? "https://www.c-sharpcorner.com" + item.Elements().First(i => i.Name.LocalName == "link").Value : item.Elements().First(i => i.Name.LocalName == "link").Value,
+                                  PubDate = Convert.ToDateTime(item.Elements().First(i => i.Name.LocalName == "pubDate").Value, culture),
+                                  PublishDate = Convert.ToDateTime(item.Elements().First(i => i.Name.LocalName == "pubDate").Value, culture).ToString("dd-MMM-yyyy"),
+                                  Title = item.Elements().First(i => i.Name.LocalName == "title").Value,
+                                  FeedType = item.Elements().First(i => i.Name.LocalName == "link").Value.ToLowerInvariant().Contains("blog") ? "Blog" : item.Elements().First(i => i.Name.LocalName == "link").Value.ToLowerInvariant().Contains("news") ? "News" : "Article",
+                                  Author = item.Elements().First(i => i.Name.LocalName == "author").Value
+                              };
+
+                authorPosts.AuthorName = entries.FirstOrDefault().Author;
+                authorPosts.Feeds = entries.OrderByDescending(o => o.PubDate);
+                return authorPosts;
+            }
+            catch
+            {
+                authorPosts.AuthorName = "NOT FOUND!";
+                List<Feed> feeds = new List<Feed>();
+                Feed feed = new Feed();
+                feeds.Add(feed);
+                authorPosts.Feeds = feeds;
+                return authorPosts;
+            }
+        }
+
+        [Route("latestposts")]
+        [HttpGet]
+        public IEnumerable<Feed> LatestPosts()
+        {
+            try
+            {
+                XDocument doc = XDocument.Load("https://www.c-sharpcorner.com/rss/latestcontentall.aspx");
+                var entries = from item in doc.Root.Descendants().First(i => i.Name.LocalName == "channel").Elements().Where(i => i.Name.LocalName == "item")
+                              select new Feed
+                              {
+                                  Content = item.Elements().First(i => i.Name.LocalName == "description").Value,
+                                  Link = item.Elements().First(i => i.Name.LocalName == "link").Value.StartsWith("/") ? "https://www.c-sharpcorner.com" + item.Elements().First(i => i.Name.LocalName == "link").Value : item.Elements().First(i => i.Name.LocalName == "link").Value,
+                                  PubDate = Convert.ToDateTime(item.Elements().First(i => i.Name.LocalName == "pubDate").Value, culture),
+                                  PublishDate = Convert.ToDateTime(item.Elements().First(i => i.Name.LocalName == "pubDate").Value, culture).ToString("dd-MMM-yyyy"),
+                                  Title = item.Elements().First(i => i.Name.LocalName == "title").Value,
+                                  FeedType = item.Elements().First(i => i.Name.LocalName == "link").Value.ToLowerInvariant().Contains("blog") ? "Blog" : item.Elements().First(i => i.Name.LocalName == "link").Value.ToLowerInvariant().Contains("news") ? "News" : "Article",
+                                  Author = item.Elements().First(i => i.Name.LocalName == "author").Value
+                              };
+
+                return entries.OrderByDescending(o => o.PubDate);
+            }
+            catch
+            {
+                List<Feed> feeds = new List<Feed>();
+                Feed feed = new Feed();
+                feeds.Add(feed);
+                return feeds;
+            }
+        }
+
+        [Route("featuredposts")]
+        [HttpGet]
+        public IEnumerable<Feed> FeaturedPosts()
+        {
+            try
+            {
+                XDocument doc = XDocument.Load("https://www.c-sharpcorner.com/rss/featuredarticles.aspx");
+                var entries = from item in doc.Root.Descendants().First(i => i.Name.LocalName == "channel").Elements().Where(i => i.Name.LocalName == "item")
+                              select new Feed
+                              {
+                                  Content = item.Elements().First(i => i.Name.LocalName == "description").Value,
+                                  Link = item.Elements().First(i => i.Name.LocalName == "link").Value.StartsWith("/") ? "https://www.c-sharpcorner.com" + item.Elements().First(i => i.Name.LocalName == "link").Value : item.Elements().First(i => i.Name.LocalName == "link").Value,
+                                  PubDate = Convert.ToDateTime(item.Elements().First(i => i.Name.LocalName == "pubDate").Value, culture),
+                                  PublishDate = Convert.ToDateTime(item.Elements().First(i => i.Name.LocalName == "pubDate").Value, culture).ToString("dd-MMM-yyyy"),
+                                  Title = item.Elements().First(i => i.Name.LocalName == "title").Value,
+                                  FeedType = item.Elements().First(i => i.Name.LocalName == "link").Value.ToLowerInvariant().Contains("blog") ? "Blog" : item.Elements().First(i => i.Name.LocalName == "link").Value.ToLowerInvariant().Contains("news") ? "News" : "Article",
+                                  Author = item.Elements().First(i => i.Name.LocalName == "author").Value
+                              };
+
+                return entries.OrderByDescending(o => o.PubDate);
+            }
+            catch
+            {
+                List<Feed> feeds = new List<Feed>();
+                Feed feed = new Feed();
+                feeds.Add(feed);
+                return feeds;
+            }
+        }
+
+        [Route("topposts")]
+        [HttpGet]
+        public IEnumerable<Feed> TrendingPosts()
+        {
+            try
+            {
+                XDocument doc = XDocument.Load("https://www.c-sharpcorner.com/rss/toparticles.aspx");
+                var entries = from item in doc.Root.Descendants().First(i => i.Name.LocalName == "channel").Elements().Where(i => i.Name.LocalName == "item")
+                              select new Feed
+                              {
+                                  Content = item.Elements().First(i => i.Name.LocalName == "description").Value,
+                                  Link = item.Elements().First(i => i.Name.LocalName == "link").Value.StartsWith("/") ? "https://www.c-sharpcorner.com" + item.Elements().First(i => i.Name.LocalName == "link").Value : item.Elements().First(i => i.Name.LocalName == "link").Value,
+                                  PubDate = Convert.ToDateTime(item.Elements().First(i => i.Name.LocalName == "pubDate").Value, culture),
+                                  PublishDate = Convert.ToDateTime(item.Elements().First(i => i.Name.LocalName == "pubDate").Value, culture).ToString("dd-MMM-yyyy"),
+                                  Title = item.Elements().First(i => i.Name.LocalName == "title").Value,
+                                  FeedType = item.Elements().First(i => i.Name.LocalName == "link").Value.ToLowerInvariant().Contains("blog") ? "Blog" : item.Elements().First(i => i.Name.LocalName == "link").Value.ToLowerInvariant().Contains("news") ? "News" : "Article",
+                                  Author = item.Elements().First(i => i.Name.LocalName == "author").Value
+                              };
+
+                return entries;
+            }
+            catch
+            {
+                List<Feed> feeds = new List<Feed>();
+                Feed feed = new Feed();
+                feeds.Add(feed);
+                return feeds;
+            }
+        }
+
+
+    }
+}
---
+++ MSSAProjectTwo/MSSAProjectTwo/Shared/Models/RSSFeedController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text;
+using System.Xml;
+using System.ServiceModel.Syndication;
+
+namespace MSSAProjectTwo.Shared.Models
+{
+    public class RSSFeedController : ControllerBase
+    {
+        [ResponseCache(Duration = 1200)]
+        [HttpGet]
+        [Route("feed.rss")]
+        public async Task<IActionResult> GetRssFeed()
+        {
+            return Accepted();
+        }
+    }
+
+    public class AuthorPosts
+    {
+        public string AuthorName { get; set; }
+        public IEnumerable<Feed> Feeds { get; set; }
+    }
+
+    public class Feed
+    {
+        public Feed()
+        {
+            Link = "";
+            Title = "";
+            FeedType = "";
+            Author = "";
+            Content = "";
+            PubDate = DateTime.Today;
+            PublishDate = DateTime.Today.ToString("yyyy-MM-dd");
+        }
+
+        public string Link { get; set; }
+        public string Title { get; set; }
+        public string FeedType { get; set; }
+        public string Author { get; set; }
+        public string Content { get; set; }
+        public DateTime PubDate { get; set; }
+        public string PublishDate { get; set; }
+    }
+}
