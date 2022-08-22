3d6ecf4aa62885f56541a5d44df7032cbe0b17e5
Nate Berry <78959107+berrynp76@users.noreply.github.com>
2022-08-22 13:06:41 -0700

    Restarted the second MSSA project with MAUI instead of Blazor

---
+++ ProjectTwoMSSA/ProjectTwoMSSA/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace ProjectTwoMSSA;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ ProjectTwoMSSA/ProjectTwoMSSA/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace ProjectTwoMSSA;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
