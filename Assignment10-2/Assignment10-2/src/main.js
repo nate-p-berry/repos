236782bfa1afaf12fc81a32b9295267edfabd3e1
Nate Berry <78959107+nate-p-berry@users.noreply.github.com>
2022-09-15 09:22:50 -0700

    Installed NPM

---
+++ Assignment10-2/Assignment10-2/src/main.js
@@ -0,0 +1,16 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+
+createApp(App).mount('#app')
+
+// Language: typescript
+// Path: Assignment10-2\src\App.vue
+    < template >
+    <div id="app">
+        <h1>Assignment 10-2</h1>
+        <h2>My name is {{ name }}</h2>
+        <h2>My age is {{ age }}</h2>
+        <h2>My  address  is {{ address }}</h2>
+        <h2>My  phone  number  is {{ phone }}</h2>
+        <h2>My  email  i s {{ email }}</h2>
+    </div>
