04b42355b886949871861b4d26f15439c004d7d3
Nate Berry <78959107+nate-p-berry@users.noreply.github.com>
2022-09-30 11:21:19 -0700

    20220930

---
+++ Practice/Practice/IdentificationNumber.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice
+{
+    internal class IdentificationNumber
+    {
+        int _id;
+
+        public IdentificationNumber(int id)
+        {
+            _id = id;
+        }
+
+        public static string GetId(IdentificationNumber id)
+        {
+            return id._id.ToString();
+        }
+
+        public static string CreateId(int position)
+        {
+            char[] newId = new char[5];
+            char[] primes = string.Concat(PrimeNumbers.primeNumbers).ToCharArray();
+            string primeString = primes.ToString();
+            for (int i = 0; i < 5; i++)
+            {
+                newId[i] = primes[position + i];
+            }
+            return string.Concat(newId);
+        }
+    }
+}
